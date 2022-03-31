94f9ab00d9891b46b45592a80fa12f63dcc20bc5
plamenipenchev <plamen.ivanov.penchev@gmail.com>
2022-04-01 00:07:44 +0300

    Initial Commit

---
+++ auth/src/index.ts
@@ -0,0 +1,52 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler } from "./middlewares/error-handler";
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT has to be defined!");
+  }
+
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connectingto DB!");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(30570, () => {
+    console.log("Listening on port 30570!!!");
+  });
+};
+
+start();
---
+++ auth/src/routes/signin.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
+
+const router = express.Router();
+
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid Credentials!");
+    }
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError("Invalid Credentials!");
+    }
+  }
+);
+
+export { router as signinRouter };
